e7c35d905b9233d8f07cb937a39e5cca43474c72
SophieM <47410186+SophM@users.noreply.github.com>
2019-04-04 21:08:02 -0700

    add the code in connection.js to access the database when the app is deployed to Heroku

--- config/connection.js
+++ config/connection.js
@@ -10,16 +10,21 @@ var mysql = require("mysql");
 // Set up the connection to mySQL and export
 // --------------------------------------------------- 
 
-// to deploy to Heroku - add something here!!
-// configure the connection with my credentials
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "a9uLc2frfg7buEidFA",
-    database: "oneDay_db"
-}); 
- 
+// connect to JawsDB to access oneDay_db when the deployed version is used
+if(process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+// when used locally
+} else {
+    // connect to the localhost with my credentials
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "a9uLc2frfg7buEidFA",
+        database: "oneDay_db"
+    });
+}
+
 // open the connection with oneDay_db
 connection.connect(function(err) {
     if (err) {
